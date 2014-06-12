9ac2339c4da25049d8912ba795d1940ba6a22217
Vaughan Hilts <hilts.vaughan@gmail.com>
2014-06-12 19:28:35 -0400

    Implemented mongoose on the app.js

--- otaku-scraper/app.js
+++ otaku-scraper/app.js
@@ -12,38 +12,56 @@ var api = global.api,
   AniChart = require('./lib/api/anichart/anichart'),
   News = require('./lib/api/mal/news');
 
+// Load up our database stuff we need
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost/otaku');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/otaku');
 
+var db = mongoose.connection;
+db.on('error', function(err) {
+  console.log(err + " while attempting to open the database. Exiting.");
+  process.exit(1);
+});
 
+db.once('open', function callback() {
+
+  // Register API once the database has been opened properly
+  apiRegister('/mal/anime/id/:id([0-9]+)', api.mal.anime.id);
+  apiRegister('/mal/anime/name/:name', api.mal.anime.name);
+  apiRegister('/mal/news/', api.mal.news);
+  apiRegister('/anichart', api.anichart.current);
+  apiRegister('/anichart/current', api.anichart.current);
+  apiRegister('/anichart/spring', api.anichart.spring);
+  apiRegister('/anichart/summer', api.anichart.summer);
+  apiRegister('/anichart/fall', api.anichart.fall);
+  apiRegister('/anichart/winter', api.anichart.winter);
+  apiRegister('/anichart/spring/:id([0-9]+)', api.anichart.spring);
+  apiRegister('/anichart/summer/:id([0-9]+)', api.anichart.summer);
+  apiRegister('/anichart/fall/:id([0-9]+)', api.anichart.fall);
+  apiRegister('/anichart/winter/:id([0-9]+)', api.anichart.winter);
+
+});
+
+// Setup express
 var app = express();
 if ('test' !== app.get('env')) app.use(express.logger(config.get('logger:format')));
 app.use(express.responseTime());
 app.use(express.favicon());
 app.use(express.json());
+
+// Allow cross origin requests
 var cors = require('cors');
 app.use(cors());
 
 // Make our db accessible to our router
 app.use(function(req, res, next) {
+
+  // Patch the requests incoming
   req.db = db;
+
   next();
 });
 
-apiRegister('/mal/anime/id/:id([0-9]+)', api.mal.anime.id);
-apiRegister('/mal/anime/name/:name', api.mal.anime.name);
-apiRegister('/mal/news/', api.mal.news);
-apiRegister('/anichart', api.anichart.current);
-apiRegister('/anichart/current', api.anichart.current);
-apiRegister('/anichart/spring', api.anichart.spring);
-apiRegister('/anichart/summer', api.anichart.summer);
-apiRegister('/anichart/fall', api.anichart.fall);
-apiRegister('/anichart/winter', api.anichart.winter);
-apiRegister('/anichart/spring/:id([0-9]+)', api.anichart.spring);
-apiRegister('/anichart/summer/:id([0-9]+)', api.anichart.summer);
-apiRegister('/anichart/fall/:id([0-9]+)', api.anichart.fall);
-apiRegister('/anichart/winter/:id([0-9]+)', api.anichart.winter);
 
 app.use(notfound());
 
